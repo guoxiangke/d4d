7e1c7ec7f581d45105111c71197ab76e853487a5
dale <dale.guo@mullenloweprofero.com>
2017-05-10 15:20:27 +0800

    add haproxy

---
+++ README.md
@@ -0,0 +1,10 @@
+# 欢迎使用 d4d (Docker4Drupal)
+
+------
+
+```bash
+#Usage:
+$ chmod +x ./d4d.sh
+$ ./d4d.sh simple #build a simple one
+$ ./d4d.sh multi  #build with haproxy
+```
---
+++ d4d.sh
@@ -0,0 +1,14 @@
+#!  /bin/bash 
+#author : dale.tel@139.com
+# require: docker-compose installed
+
+# #install docker-compse
+# docker-compose-version= "1.13.0";
+# docker-compose-sh-url= "https://github.com/docker/compose/releases/download/${docker-compose-version}/run.sh"
+# sudo curl -L "${docker-compose-sh-url}" > /usr/local/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
+
+#build & start  drupal !!!
+composeFile= $1
+projectName= "d4d_${composeFile}"
+docker-compose -p "${projectName} " -f "docker-compose-${composeFile}.yml " up -d 
