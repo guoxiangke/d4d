3b80b7143ce12c526ad13a5415ff196599ccd44a
dale <dale.guo@mullenloweprofero.com>
2017-05-10 17:21:43 +0800

    up sh

--- d4d.sh
+++ d4d.sh
@@ -1,14 +1,36 @@
 #!  /bin/bash 
 #author : dale.tel@139.com
 # require: docker-compose installed
+trap 'echo “before execute line:$LINENO, composeFile=$composeFile,projectName”=$projectName”' DEBUG
 
-# #install docker-compse
-# docker-compose-version= "1.13.0";
-# docker-compose-sh-url= "https://github.com/docker/compose/releases/download/${docker-compose-version}/run.sh"
-# sudo curl -L "${docker-compose-sh-url}" > /usr/local/bin/docker-compose
-# sudo chmod +x /usr/local/bin/docker-compose
+file="/usr/local/bin/docker-compose"
+if [ -f $file ]
+then
+	##build & start  drupal !!!
+	composeFile=$1
+	if [ composeFile ] 
+	then
+		if [ composeFile = "simple" ] 
+		then
+			projectName="d4d_${composeFile}"
+		else
+			projectName="d4d_haproxy"
+		fi
+		compose_yml="./docker-compose-${composeFile}.yml"
+		if [ -f $compose_yml ]
+		then
+			docker-compose -p "${projectName} " -f compose_yml up -d 
+		else
+			echo "Error: No ${compose_yml}!"
+		fi
+	else
+		echo "Error: Wrong pargrams!"
+	fi
+else
+	# #install docker-compse
+	docker_compose_version="1.13.0";
+	docker_compose_sh_url="https://github.com/docker/compose/releases/download/${docker_compose_version}/run.sh"
+	curl -L "${docker_compose_sh_url}" > /usr/local/bin/docker-compose
+	chmod +x /usr/local/bin/docker-compose
+fi
 
-#build & start  drupal !!!
-composeFile=$1
-projectName="d4d_${composeFile}"
-docker-compose -p "${projectName} " -f "docker-compose-${composeFile}.yml " up -d 
