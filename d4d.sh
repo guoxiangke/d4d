b9a5f89ddb33ba5d00be351ffed717e100d2a16c
dale <dale.guo@mullenloweprofero.com>
2017-05-12 14:59:17 +0800

    up

--- d4d.sh
+++ d4d.sh
@@ -16,7 +16,15 @@ then
 		then
 			docker-compose -p ${projectName}  -f ${compose_yml} up -d 
 			echo "If not build plz execute blow cmd\r\n"
-			echo "sudo docker-compose -p ${projectName}  -f ${compose_yml}  up -d "
+			# --force-rm 删除构建过程中的临时容器。
+			# --no-cache 构建镜像过程中不使用 cache（这将加长构建过程）。
+			# --pull 始终尝试通过 pull 来获取更新版本的镜像。
+			#默认情况，如果服务容器已经存在，docker-compose up 将会尝试停止容器，然后重新创建（保持使用 volumes-from 挂载的卷），
+			# 以保证新启动的服务匹配 docker-compose.yml 文件的最新内容。如果用户不希望容器被停止并重新创建，
+			# 可以使用 docker-compose up --no-recreate。这样将只会启动处于停止状态的容器，而忽略已经运行的服务。
+			# 如果用户只想重新部署某个服务，可以使用 docker-compose up --no-deps -d <SERVICE_NAME> 
+			# 来重新创建服务并后台停止旧服务，启动新服务，并不会影响到其所依赖的服务。
+			echo "sudo docker-compose --pull --force-rm -p ${projectName}  -f ${compose_yml}  up -d "
 		else
 			echo "Error: No ${compose_yml} !"
 		fi
